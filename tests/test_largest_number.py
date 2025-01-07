4f56cff235e90ee24cd9304bebdd391707f46fb4
adupoku71 <davipoku71@gmail.com>
2025-01-07 12:34:37 +0000

    test for largest number in an array of integers

---
+++ tests/test_largest_number.py
@@ -0,0 +1,10 @@
+from Arrays.largest_number_in_array import find_largest
+
+def test_find_largest_number_1():
+    assert find_largest([10, 20, 4]) == 20
+    [20, 10, 20, 4, 100]
+    
+
+
+def test_find_largest_number_2():
+    assert find_largest([20, 10, 20, 4, 100]) == 100
