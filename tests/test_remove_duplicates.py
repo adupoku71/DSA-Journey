58bc04f4795f3e91d2318863959bf7f4f1e0596e
adupoku71 <davipoku71@gmail.com>
2025-02-15 17:33:15 +0000

    updated test for remove dupicates

--- tests/test_remove_duplicates.py
+++ tests/test_remove_duplicates.py
@@ -3,5 +3,5 @@ from Arrays.remove_duplicates_from_sorted_array import remove_duplicates, remove
 def test_remove_duplicates():
     arr = [1, 2, 2, 3, 4, 4, 4, 5, 5]
     
-    assert remove_duplicates_optimised(arr) == 5
-    assert remove_duplicates(arr) == 5
+    assert remove_duplicates_optimised(arr) == len(set(arr))
+    assert remove_duplicates(arr) == len(set(arr))
